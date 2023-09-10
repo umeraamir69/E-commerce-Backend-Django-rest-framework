afed0123478719de42eb8b145235f57102fd16fe
Muhammad Umer Aamir <93091750+umeraamir69@users.noreply.github.com>
2023-09-11 01:40:23 +0500

    category final

--- category/urls.py
+++ category/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('' , views.category , name="Categories"),
+    path('/featuer' , views.getFeature , name="Feature Categories"),
     path('<int:id>' , views.getCategoryById , name="Get Category by ID"),
     path('<slug:slug>' , views.getCategoryBySlug , name="Get Category by Slug"),
 ]
